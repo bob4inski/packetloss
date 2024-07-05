ecd8f05251736a532a96ca5b4e192ca681dbbd8c
bob4inski <robertstryzhak@yandex.ru>
2024-07-05 17:16:35 +0300

    udp

--- window.py
+++ window.py
@@ -1,8 +1,6 @@
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
-from scapy.all import rdpcap
-from scapy.layers.inet import TCP
 
 def extract_window_size(info):
     if isinstance(info, str) and "Win=" in info:
@@ -14,28 +12,31 @@ def extract_window_size(info):
             return int(info[start:start+end])
     return None
 
-csv_file = 'csv/s_bbr_c_bbr_loss0.csv'
+csv_file = '../converted/tcpdump-s_bbr_c_bbr_loss6.pcap.csv'
 
-df = pd.read_csv(csv_file)
+df = pd.read_csv(csv_file,delimiter=";")
+df.rename(columns={'_ws.col.Time': 'Time', '_ws.col.Source': 'SRC',"_ws.col.Info":"Info",'_ws.col.Protocol': 'Protocol'}, inplace=True)
 filtered_df = df[df['Protocol'] == "TCP"]
-print(filtered_df)
 
-filtered_df = filtered_df.reset_index()  # make sure indexes pair with number of rows
-filtered_df['window_size'] = filtered_df['Info'].apply(extract_window_size)
-filtered_df = filtered_df.dropna(subset=['window_size'])
+out_df = filtered_df[filtered_df['SRC'] == "10.10.10.10"]
+in_df = filtered_df[filtered_df['SRC'] == "10.10.10.20"]
+fig, axs = plt.subplots(1, 2, figsize=(15, 10))  # Create a 2x2 grid of subplots
+axs = axs.ravel()  # Flatten the array of axes for easy iteration
 
-timestamps = filtered_df['Time']
-window_sizes = filtered_df['window_size']
+for i, df in enumerate([out_df,in_df]):
+    df = df.reset_index()  # make sure indexes pair with number of rows
+    df['window_size'] = filtered_df['Info'].apply(extract_window_size)
+    df = df.dropna(subset=['window_size'])
 
-# # Make timestamps relative to the first timestamp
-initial_time = timestamps.min()
-relative_timestamps = timestamps - initial_time
+    timestamps = df['Time']
+    window_sizes = df['window_size']
+    # # Make timestamps relative to the first timestamp
+    initial_time = timestamps.min()
+    relative_timestamps = timestamps - initial_time
+    ax = axs[i]
 
-plt.figure(figsize=(12, 6))
-plt.plot(relative_timestamps, window_sizes, marker='o', linestyle='-')
-plt.xlabel('Time (seconds)')
-plt.ylabel('TCP Window Size')
-plt.title('TCP Window Scaling over Time')
-plt.grid(True)
-plt.savefig('tcp_window_scaling_plot.png')
+    ax.plot(relative_timestamps,window_sizes, marker='o', linestyle='-')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Time')
+    ax.set_title(f'Time')
 plt.show()
